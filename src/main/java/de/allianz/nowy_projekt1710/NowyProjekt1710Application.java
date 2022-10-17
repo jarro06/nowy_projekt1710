fec5a995e4df2030bb15719434b4c70290f309e0
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-17 11:59:46 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/nowy_projekt1710/NowyProjekt1710Application.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_projekt1710;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowyProjekt1710Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowyProjekt1710Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/nowy_projekt1710/NowyProjekt1710ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.nowy_projekt1710;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowyProjekt1710ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
